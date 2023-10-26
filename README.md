d43804142692657c37b0e07e0f4418529828df4a
downloadjpg <brody.foster@gmail.com>
2023-10-25 20:23:17 -0400

    reorganization and readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# brooks-research
-Filtering for Dr. Brooks equine genetics research
+## Hi! 
+This is to filter horse statistics from a PDF format into a spreadsheet using some simple Python code.
+
+We are only sampling from horses with 3 or more starts.
+
+## From each PDF we need:
+- the name of the foal
+- how many starts they had
+- the year they were born
+- sex
+- dam (mother) name
+- dam sire (if possible)
+- totals (how many win place and shows)
+- career length (in years?)
+
+![Diagram of PDF with circled fields.](u2gtdmkj.bmp)
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Foal:
 
 
 def main():
-    reader = PdfReader("AwesomeAgain.pdf")
+    reader = PdfReader("input/AwesomeAgain.pdf")
     # if you're wondering how i did this, i don't know either. regex is wizard shit.
     #pattern = r'((?!=)[^\n\\]{1,23}), ([^,]+),( [\w\s\/]+)? (colt|gelding|filly) -- ([^\(]+) \((\d+)\)(?: \(SPR=(\d+); CPI=(\d+\.\d+))?'
     # todo: add dam_sire_year to regex, need to figure out the country code + year thing
@@ -32,7 +32,7 @@ def main():
         matches = re.findall(pattern, page.extract_text())
 
         for match in matches:
-            name, birthday, color, sex, dam, dam_year, dam_spr, dam_cpi, dam_sire, dam_sire_year = match
+            name, birthday, sex, dam, dam_year, dam_spr, dam_cpi, dam_sire, dam_sire_year = match
             foals.append(Foal(name, birthday, sex, dam, dam_year, dam_spr, dam_cpi, dam_sire, dam_sire_year))
 
     print("writing file...")
